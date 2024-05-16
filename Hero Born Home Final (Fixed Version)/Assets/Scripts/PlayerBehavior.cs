32584f2d08b9b89113c5f012d40f8c96d5fa4fb7
AR0dri04 <arodrig083@citymail.cuny.edu>
2024-05-16 00:33:37 -0400

    Added Jump and Shoot Mechanics From Chapter 8
    Note: Although the code was copied exactly as it is in the textbook, when jumping with space, you can still jump an infinite amount of times. Look into why this is happening and what to fix in the playerbehavior script.

--- Hero Born Home Final (Fixed Version)/Assets/Scripts/PlayerBehavior.cs
+++ Hero Born Home Final (Fixed Version)/Assets/Scripts/PlayerBehavior.cs
@@ -15,9 +15,24 @@ public class PlayerBehavior : MonoBehaviour
 
     private Rigidbody _rb;
 
+    public float JumpVelocity = 5f;
+    private bool _isJumping;
+
+    public float DistanceToGround = 0.1f;
+    public LayerMask GroundLayer;
+    private CapsuleCollider _col;
+
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+    private bool _isShooting;
+
     void Start()
     {
+        //Here we define the Rigidbody Component in our game
         _rb = GetComponent<Rigidbody>();
+
+        //Here we define our Capsule Collider Component
+        _col = GetComponent<CapsuleCollider>();
     }
 
     // Update is called once per frame
@@ -27,8 +42,12 @@ public class PlayerBehavior : MonoBehaviour
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         //4
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
+
+        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+
+        _isShooting |= Input.GetKeyDown(KeyCode.C);
         /*
-        //5
+        //5 The following code is commented out as we use more efficient code further down
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
         //6
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
@@ -45,5 +64,37 @@ public class PlayerBehavior : MonoBehaviour
         _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
         //5
         _rb.MoveRotation(_rb.rotation * angleRot);
+
+        if(_isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+        _isJumping = false;
+
+        if(IsGrounded() && _isJumping)
+        {
+            _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
+        }
+
+        if(_isShooting)
+        {
+            GameObject newBullet = Instantiate(Bullet, this.transform.position + new Vector3(0, 0, 1), this.transform.rotation);
+            
+            Rigidbody BulletRB = newBullet.GetComponent<Rigidbody>();
+
+            BulletRB.velocity = this.transform.forward * BulletSpeed;
+        }
+        _isShooting = false;
+        
+
+
+    }
+    private bool IsGrounded()
+    {
+        Vector3 capsuleBottom = new Vector3(_col.bounds.center.x, _col.bounds.min.y, _col.bounds.center.z);
+
+        bool grounded = Physics.CheckCapsule(_col.bounds.center, capsuleBottom, DistanceToGround, GroundLayer, QueryTriggerInteraction.Ignore);
+        
+        return grounded;
     }
 }
